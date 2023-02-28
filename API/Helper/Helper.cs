7b9a0d189cd2e4fff3ca2eb3b8e9a451aa8acd06
quan <ngohongquan98tb@gmail.com>
2023-02-28 09:40:00 +0700

    update helper.cs

--- API/Helper/Helper.cs
+++ API/Helper/Helper.cs
@@ -1698,5 +1698,54 @@ namespace Helper
             });
         }
         #endregion
+
+        #region Check SQL Injection
+        public static Boolean checkForSQLInjection(string userInput)
+        {
+            bool isSQLInjection = false;
+            string[] sqlCheckList = { "--",
+                                      ";--",
+                                      ";",
+                                      "/*",
+                                      "*/",
+                                      "@@",
+                                      "@",
+                                      "char",
+                                      "nchar",
+                                      "varchar",
+                                      "nvarchar",
+                                      "alter",
+                                      "begin",
+                                      "cast",
+                                      "create",
+                                      "cursor",
+                                      "declare",
+                                      "delete",
+                                      "drop",
+                                      "end",
+                                      "exec",
+                                      "execute",
+                                      "fetch",
+                                      "insert",
+                                      "kill",
+                                      "select",
+                                      "sys",
+                                      "sysobjects",
+                                      "syscolumns",
+                                      "table",
+                                      "update"
+                                    };
+            string CheckString = userInput.Replace("'", "''");
+            for (int i = 0; i <= sqlCheckList.Length - 1; i++)
+            {
+                if ((CheckString.IndexOf(sqlCheckList[i], StringComparison.OrdinalIgnoreCase) >= 0))
+                {
+                    isSQLInjection = true;
+                }
+            }
+            return isSQLInjection;
+        }
+        #endregion
+
     }
 }
