be35e27a99dc130d34a7ea0cc57bca308b51eb10
Mysterious MitiS <haisesasaki1203@gmail.com>
2023-03-01 17:12:37 +0700

    add

--- Vue/vite.config.js
+++ Vue/vite.config.js
@@ -5,32 +5,31 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        // legacy({
-        //     targets: ['chrome >= 64', 'edge >= 79', 'safari >= 11.1', 'firefox >= 67'],
-        //     ignoreBrowserslistConfig: true,
-        //     renderLegacyChunks: false,
-        //     modernPolyfills: ['es/global-this'],
-        //     //  or
-        //     // modernPolyfills: true,
-        // }),
-        vue()
-    ],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src",
-                import.meta.url)),
-        },
+  plugins: [
+    // legacy({
+    //     targets: ['chrome >= 64', 'edge >= 79', 'safari >= 11.1', 'firefox >= 67'],
+    //     ignoreBrowserslistConfig: true,
+    //     renderLegacyChunks: false,
+    //     modernPolyfills: ['es/global-this'],
+    //     //  or
+    //     // modernPolyfills: true,
+    // }),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
 
   // Local test
-  define: {
-      baseURL: JSON.stringify("https://localhost:44393/"),
-      fileURL: JSON.stringify("https://localhost:44393/"),
-      socketURL: JSON.stringify("https://socket2.soe.vn/"),
-      SecretKey: JSON.stringify("1012198815021989"),
-      isDev: true,
-  },
+  // define: {
+  //     baseURL: JSON.stringify("https://localhost:44393/"),
+  //     fileURL: JSON.stringify("https://localhost:44393/"),
+  //     socketURL: JSON.stringify("https://socket2.soe.vn/"),
+  //     SecretKey: JSON.stringify("1012198815021989"),
+  //     isDev: true,
+  // },
   // Của BHBQP
   // define: {
   //     baseURL: JSON.stringify("http://172.16.102.211:8080/"),
@@ -53,13 +52,13 @@ export default defineConfig({
   //     SecretKey: JSON.stringify("1012198815021989"),
   //     isDev: true,
   // },
-  // define: {
-  //   baseURL: JSON.stringify("https://apiv2.soe.vn/"),
-  //   fileURL: JSON.stringify("https://apiv2.soe.vn/"),
-  //   socketURL: JSON.stringify("https://socket2.soe.vn/"),
-  //   SecretKey: JSON.stringify("1012198815021989"),
-  //   isDev: true,
-  // },
+  define: {
+    baseURL: JSON.stringify("https://apiv2.soe.vn/"),
+    fileURL: JSON.stringify("https://apiv2.soe.vn/"),
+    socketURL: JSON.stringify("https://socket2.soe.vn/"),
+    SecretKey: JSON.stringify("1012198815021989"),
+    isDev: true,
+  },
   server: {
     host: true,
     //port 3000 localhost
