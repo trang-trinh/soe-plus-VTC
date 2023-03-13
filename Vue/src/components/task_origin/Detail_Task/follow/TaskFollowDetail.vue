7e8f83133f39e9024c75d935ea91178e1b5948bc
Mysterious MitiS <haisesasaki1203@gmail.com>
2023-03-13 16:21:08 +0700

    add new view Hoang

---
+++ Vue/src/components/task_origin/Detail_Task/follow/TaskFollowDetail.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref, inject, onMounted, watch } from "vue";
+import { required } from "@vuelidate/validators";
+import { useToast } from "vue-toastification";
+import { encr } from "../../../../util/function.js";
+import moment from "moment";
+import { FilterMatchMode, FilterOperator } from "primevue/api";
+import useVuelidate from "@vuelidate/core";
+const cryoptojs = inject("cryptojs");
+const emitter = inject("emitter");
+const axios = inject("axios");
+const store = inject("store");
+const toast = useToast();
+const swal = inject("$swal");
+// eslint-disable-next-line no-undef
+const basedomainURL = baseURL;
+const config = {
+  headers: { Authorization: `Bearer ${store.getters.token}` },
+};
+const width1 = window.screen.width;
+const addLog = (log) => {
+  // eslint-disable-next-line no-undef
+  axios.post(baseURL + "/api/Proc/AddLog", log, config);
+};
+const bgColor = ref([
+  "#F4B2A3",
+  "#F8E69A",
+  "#AFDFCF",
+  "#F4B2A3",
+  "#9A97EC",
+  "#CAE2B0",
+  "#8BCFFB",
+  "#CCADD7",
+]);
+let user = store.state.user;
+const datalists = ref([]);
+const options = ref({
+  loading: true,
+});
+const listStatus = ref([
+  {
+    value: 0,
+    text: "Chưa bắt đầu",
+    bg_color: "#bbbbbb",
+    text_color: "#FFFFFF",
+  },
+  { value: 1, text: "Đang làm", bg_color: "#2196f3", text_color: "#FFFFFF" },
+  { value: 2, text: "Tạm ngừng", bg_color: "#d87777", text_color: "#FFFFFF" },
+  { value: 3, text: "Đã đóng", bg_color: "#d87777", text_color: "#FFFFFF" },
+  { value: 4, text: "HT đúng hạn", bg_color: "#04D215", text_color: "#FFFFFF" },
+  {
+    value: 5,
+    text: "Chờ đánh giá",
+    bg_color: "#33c9dc",
+    text_color: "#FFFFFF",
+  },
+  { value: 6, text: "Bị trả lại", bg_color: "#ffa500", text_color: "#FFFFFF" },
+  { value: 7, text: "HT sau hạn", bg_color: "#ff8b4e", text_color: "#FFFFFF" },
+  { value: 8, text: "Đã đánh giá", bg_color: "#51b7ae", text_color: "#FFFFFF" },
+  { value: -1, text: "Bị xóa", bg_color: "red", text_color: "#FFFFFF" },
+]);
+const props = defineProps({
+  data: Object,
+});
+onMounted(() => {});
+</script>
+<template>
+  <div>{{ props.data }}</div>
+</template>
+
+<style lang="scss" scoped></style>
