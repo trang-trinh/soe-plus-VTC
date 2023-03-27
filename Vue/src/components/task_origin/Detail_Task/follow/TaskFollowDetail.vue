49b9f6eaa5b81df9897006de8f435d7e54b92aa2
Mysterious MitiS <haisesasaki1203@gmail.com>
2023-03-27 15:17:55 +0700

    commit

--- Vue/src/components/task_origin/Detail_Task/follow/TaskFollowDetail.vue
+++ Vue/src/components/task_origin/Detail_Task/follow/TaskFollowDetail.vue
@@ -47,12 +47,13 @@ const props = defineProps({
   rowReorder: Function,
   memberType: Intl,
 });
+const type_display = ref("1");
 onMounted(() => {});
 </script>
 <template>
   <Dialog
     v-model:visible="props.isOpen"
-    :style="'width:88vw;'"
+    :style="'width:90vw;'"
     :showCloseIcon="true"
     :header="'Chi tiết quy trình'"
     @update:visible="closeDialogDetail"
@@ -170,7 +171,7 @@ onMounted(() => {});
                 </div>
               </div>
               <div class="col-12 flex">
-                <div class="col-1">Mô tả quy trình</div>
+                <div class="col-1">Mô tả quy trình:</div>
                 <div
                   class="col-11"
                   v-html="props.data.description"
@@ -188,6 +189,7 @@ onMounted(() => {});
       </div>
 
       <DataTable
+        v-if="type_display == 1"
         :value="props.data.task_follow_step"
         v-model:expandedRows="expandedRows"
       >
@@ -206,8 +208,8 @@ onMounted(() => {});
           field="step_name"
           header-class="justify-content-center align-items-center text-center"
         >
-          <template #body="data"
-            ><div>
+          <template #body="data">
+            <div>
               <span class="font-bold text-xl">
                 {{ data.data.step_name }}
               </span>
@@ -302,45 +304,135 @@ onMounted(() => {});
                   Thông tin bước:
                   <span class="px-1 font-bold line-height-3">{{
                     slotProps.data.step_name
-                  }}</span></span
-                >
+                  }}</span>
+                  ádsd
+                </span>
               </template>
             </Toolbar>
-            <Accordion
-              :activeIndex="0"
-              class="w-full"
-            >
-              <AccordionTab header="Mô tả bước">
-                <span
+            <Accordion :activeIndex="0">
+              <AccordionTab header="Thông tin chung">
+                <div
                   class="max-h-15rem"
                   style="overflow-y: auto"
                 >
-                  {{ slotProps.data.description }}
-                  <span
-                    v-html="slotProps.data.description"
+                  <div
+                    class="col-12 flex"
+                    v-if="slotProps.data.start_date || slotProps.data.end_date"
+                  >
+                    <div
+                      class="col-6"
+                      v-if="slotProps.data.start_date"
+                    >
+                      Ngày bắt đầu(dự kiến):
+                      <span class="pl-2 text-xl text-blue-600">{{
+                        moment(new Date(slotProps.data.start_date)).format(
+                          "HH:mm DD/MM/YYYY",
+                        )
+                      }}</span>
+                    </div>
+                    <div
+                      class="col-6"
+                      v-if="slotProps.data.end_date"
+                    >
+                      Ngày kết thúc(dự kiến):
+                      <span class="pl-2 text-xl text-blue-600">{{
+                        moment(new Date(slotProps.data.end_date)).format(
+                          "HH:mm DD/MM/YYYY",
+                        )
+                      }}</span>
+                    </div>
+                  </div>
+                  <div
+                    class="col-12 flex"
                     v-if="
-                      slotProps.data.description != null &&
-                      slotProps.data.description != ''
+                      slotProps.data.start_real_date ||
+                      slotProps.data.end_real_date
                     "
-                  ></span>
-                  <b v-else>Không có mô tả cho bước này!</b>
-                </span>
+                  >
+                    <div
+                      class="col-6"
+                      v-if="slotProps.data.start_real_date"
+                    >
+                      Ngày bắt đầu:
+                      <span class="pl-2 text-xl text-blue-600">{{
+                        moment(new Date(slotProps.data.start_real_date)).format(
+                          "HH:mm DD/MM/YYYY",
+                        )
+                      }}</span>
+                    </div>
+                    <div
+                      class="col-6"
+                      v-if="slotProps.data.end_real_date"
+                    >
+                      Ngày kết thúc:
+                      <span class="pl-2 text-xl text-blue-600">{{
+                        moment(new Date(slotProps.data.end_real_date)).format(
+                          "HH:mm DD/MM/YYYY",
+                        )
+                      }}</span>
+                    </div>
+                  </div>
+                  <div class="col-12 flex">
+                    <div class="col-6">
+                      Trạng thái:
+                      <span
+                        class=""
+                        :style="{
+                          background: slotProps.data.status_display.bg_color,
+                          color: slotProps.data.status_display.text_color,
+                          padding: '2px 8px',
+                          border:
+                            '1px solid' +
+                            slotProps.data.status_display.bg_color,
+                          borderRadius: '5px',
+                        }"
+                      >
+                        {{ slotProps.data.status_display.label }}
+                      </span>
+                    </div>
+                    <div class="col-6">
+                      Trình tự thực hiện:
+                      <span
+                        class=""
+                        :style="{
+                          background: slotProps.data.type_display.bg_color,
+                          color: slotProps.data.type_display.text_color,
+                          padding: '2px 8px',
+                          border:
+                            '1px solid' + slotProps.data.type_display.bg_color,
+                          borderRadius: '5px',
+                        }"
+                      >
+                        {{ slotProps.data.type_display.label }}
+                      </span>
+                    </div>
+                  </div>
+                  <div class="col-12 flex">
+                    <div class="col-1">Mô tả:</div>
+
+                    <div
+                      class="col-11"
+                      v-html="slotProps.data.description"
+                      v-if="
+                        slotProps.data.description != null &&
+                        slotProps.data.description != ''
+                      "
+                    ></div>
+                    <b
+                      class="col-11"
+                      v-else
+                      >Không có mô tả cho quy trình này!</b
+                    >
+                  </div>
+                </div>
               </AccordionTab>
             </Accordion>
 
-            <DataTable
-              :value="slotProps.data.task_info"
-              @rowReorder="props.rowReorder"
-            >
-              <Column
-                v-if="props.memberType == 0"
-                rowReorder
-                class="max-w-1rem justify-content-center align-items-center text-center"
-              />
+            <DataTable :value="slotProps.data.task_info">
               <Column
                 header="STT"
                 field="step"
-                class="justify-content-center align-items-center text-center max-w-5rem"
+                class="justify-content-center align-items-center text-center w-5rem"
               ></Column>
               <Column
                 header="Tên công việc"
@@ -374,7 +466,7 @@ onMounted(() => {});
               <Column
                 header="Tiến độ"
                 field="task_id_follow"
-                class="justify-content-center align-items-center text-center max-w-8rem"
+                class="justify-content-center align-items-center text-center w-8rem"
               >
                 <template #body="data">
                   <div v-if="data.data.progress > 0">
@@ -391,10 +483,10 @@ onMounted(() => {});
               <Column
                 header="Trạng thái"
                 field="task_id_follow"
-                class="justify-content-center align-items-center text-center max-w-8rem"
+                class="justify-content-center align-items-center text-center w-14rem"
               >
                 <template #body="data">
-                  <!-- <span
+                  <span
                     :style="{
                       background: data.data.status_display.bg_color,
                       color: data.data.status_display.text_color,
@@ -404,7 +496,7 @@ onMounted(() => {});
                     }"
                   >
                     {{ data.data.status_display.text }}
-                  </span> -->
+                  </span>
                 </template></Column
               >
 
@@ -423,6 +515,99 @@ onMounted(() => {});
           </div>
         </template>
       </DataTable>
+
+      <div
+        v-if="type_display == 2"
+        class="w-full flex"
+        style="overflow-y: auto"
+      >
+        <div
+          class="flex"
+          v-for="(item, index) in props.data.task_follow_step"
+          :key="index"
+        >
+          <Card class="flex m-3 w-45rem h-45rem">
+            <template #content>
+              <div class="align-items-center justify-content-center flex">
+                <span
+                  class="flex w-3rem h-3rem align-items-center justify-content-center text-center border-circle z-1 shadow-1 text-2xl font-bold text-blue-500"
+                >
+                  {{ item.is_step }}
+                </span>
+              </div>
+              <div class="align-items-center justify-content-center flex my-2">
+                {{ item.step_name }}
+              </div>
+              <div class="align-items-center justify-content-center flex my-2">
+                <Chip
+                  class="mx-2"
+                  :label="item.status_display.label"
+                  :style="{
+                    color: item.status_display.text_color,
+                    background: item.status_display.bg_color,
+                  }"
+                />
+                <Chip
+                  class="mx-2"
+                  :label="item.type_display.label"
+                  :style="{
+                    color: item.type_display.text_color,
+                    background: item.type_display.bg_color,
+                  }"
+                />
+              </div>
+              <div class="col-12 flex">
+                <span class="col-2">Mô tả bước:</span>
+                <div
+                  class="col-10"
+                  v-if="item.description"
+                  v-html="item.description"
+                ></div>
+                <b
+                  class="col-10"
+                  v-else
+                  >Không có mô tả cho bước này!</b
+                >
+              </div>
+              <div class="col-12 flex">
+                <div class="col-6">
+                  <span>
+                    Ngày bắt đầu(dự kiến):
+                    <span v-if="item.start_date">
+                      {{
+                        moment(new Date(item.start_date)).format("DD/MM/YYYY")
+                      }}-
+                    </span>
+
+                    <span v-if="item.is_deadline == true">
+                      {{ moment(new Date(item.end_date)).format("DD/MM/YYYY") }}
+                    </span></span
+                  >
+                </div>
+              </div>
+              <div class="col-12">{{ item }}</div>
+            </template>
+          </Card>
+
+          <icon
+            v-tooltip="'Tuần tự'"
+            class="pi pi-arrow-right font-bold text-2xl"
+            v-if="
+              props.data.type == 1 &&
+              index < props.data.task_follow_step.length - 1
+            "
+          ></icon>
+          <icon
+            class="pi pi-sort-alt font-bold text-2xl"
+            style="transform: rotate(90deg)"
+            v-tooltip="'Song song'"
+            v-if="
+              props.data.type == 2 &&
+              index < props.data.task_follow_step.length - 1
+            "
+          ></icon>
+        </div>
+      </div>
     </div>
     <!-- 
     {{ props.data.task_follow_step }} -->
@@ -434,5 +619,11 @@ onMounted(() => {});
   .p-avatar-text {
     font-size: 2rem;
   }
+}
+.w-45rem {
+  width: 45rem;
+}
+.h-45rem {
+  height: 45rem;
 }
 </style>
