a0ca0ecce91ebcf6bdc0191282c8efba11daec14
duytrangcac <it.duyls@gmail.com>
2023-03-29 10:07:07 +0700

    duy add

---
+++ API/Controllers/Sys/sys_process_link_approvedController.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using API.Helper;
+using API.Models;
+using Helper;
+using Microsoft.ApplicationBlocks.Data;
+using Newtonsoft.Json;
+namespace API.Controllers.Sys
+{
+    [Authorize(Roles = "login")]
+    public class sys_process_link_approvedController : ApiController
+    {
+
+
+        public string getipaddress()
+        {
+            return HttpContext.Current.Request.UserHostAddress;
+        }
+
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> add_sys_process_link_approved()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+            }
+            string fdconfig_process = "";
+            IEnumerable<Claim> claims = identity.Claims;
+            string ip = getipaddress();
+            string name = claims.Where(p => p.Type == "fname").FirstOrDefault()?.Value;
+            string tid = claims.Where(p => p.Type == "tid").FirstOrDefault()?.Value;
+            string dvid = claims.Where(p => p.Type == "dvid").FirstOrDefault()?.Value;
+            string uid = claims.Where(p => p.Type == "uid").FirstOrDefault()?.Value;
+
+            string domainurl = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Host + ":" + HttpContext.Current.Request.Url.Port + "/";
+
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    if (!Request.Content.IsMimeMultipartContent())
+                    {
+                        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                    }
+
+                    string root = HttpContext.Current.Server.MapPath("~/Portals");
+
+                    var provider = new MultipartFormDataStreamProvider(root);
+
+                    // Read the form data and return an async task.
+                    var task = Request.Content.ReadAsMultipartAsync(provider).
+                    ContinueWith<HttpResponseMessage>(t =>
+                    {
+                        if (t.IsFaulted || t.IsCanceled)
+                        {
+                            Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                        }
+                        fdconfig_process = provider.FormData.GetValues("sys_process_link_approved").SingleOrDefault();
+                        List< sys_process_link_approved> config_process = JsonConvert.DeserializeObject<List<sys_process_link_approved>>(fdconfig_process);
+                        bool super = claims.Where(p => p.Type == "super").FirstOrDefault()?.Value == "True";
+                        foreach (var item in config_process)
+                        {
+                            item.organization_id = super ? 0 : int.Parse(dvid);
+                            item.created_by = uid;
+                            item.created_date = DateTime.Now;
+                            item.created_ip = ip;
+                            item.created_token_id = tid;
+                            db.sys_process_link_approved.Add(item);
+                            db.SaveChanges();
+                        }
+                        return Request.CreateResponse(HttpStatusCode.OK, new { err = "0" });
+                    });
+                    return await task;
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                string contents = helper.getCatchError(e, null);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = fdconfig_process, contents }), domainurl + "sys_process_link_approved/Add_config_process", ip, tid, "Lỗi khi thêm quy trình", 0, "quy trình");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                Log.Error(contents);
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+            catch (Exception e)
+            {
+                string contents = helper.ExceptionMessage(e);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = fdconfig_process, contents }), domainurl + "sys_process_link_approved/Add_config_process", ip, tid, "Lỗi khi thêm quy trình", 0, "quy trình  ");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                Log.Error(contents);
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+        }
+        [HttpPut]
+        public async Task<HttpResponseMessage> update_sys_process_link_approved()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+            }
+            string fdconfig_process = "";
+            IEnumerable<Claim> claims = identity.Claims;
+            string ip = getipaddress();
+            string name = claims.Where(p => p.Type == "fname").FirstOrDefault()?.Value;
+            string tid = claims.Where(p => p.Type == "tid").FirstOrDefault()?.Value;
+            string dvid = claims.Where(p => p.Type == "dvid").FirstOrDefault()?.Value;
+            string uid = claims.Where(p => p.Type == "uid").FirstOrDefault()?.Value;
+            bool ad = claims.Where(p => p.Type == "ad").FirstOrDefault()?.Value == "True";
+            string domainurl = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Host + ":" + HttpContext.Current.Request.Url.Port + "/";
+            List<string> delfiles = new List<string>();
+
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    if (!Request.Content.IsMimeMultipartContent())
+                    {
+                        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                    }
+
+                    string root = HttpContext.Current.Server.MapPath("~/Portals");
+
+                    var provider = new MultipartFormDataStreamProvider(root);
+
+                    // Read the form data and return an async task.
+                    var task = Request.Content.ReadAsMultipartAsync(provider).
+                    ContinueWith<HttpResponseMessage>(t =>
+                    {
+                        if (t.IsFaulted || t.IsCanceled)
+                        {
+                            Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                        }
+                        fdconfig_process = provider.FormData.GetValues("sys_process_link_approved").SingleOrDefault();
+                        List<sys_process_link_approved> config_process = JsonConvert.DeserializeObject<List<sys_process_link_approved>>(fdconfig_process);
+                        bool super = claims.Where(p => p.Type == "super").FirstOrDefault()?.Value == "True";
+                        var i = 1;
+                        foreach (var item in config_process)
+                        {
+                            item.is_order = i;
+                            item.organization_id = super ? 0 : int.Parse(dvid);
+                            item.modified_by = uid;
+                            item.modified_date = DateTime.Now;
+                            item.modified_ip = ip;
+                            item.modified_token_id = tid;
+                            db.Entry(item).State = EntityState.Modified;
+                            db.SaveChanges();
+                            i++;
+                        }
+                 
+                    
+                        return Request.CreateResponse(HttpStatusCode.OK, new { err = "0" });
+                    });
+                    return await task;
+                }
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                string contents = helper.getCatchError(e, null);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = fdconfig_process, contents }), domainurl + "sys_process_link_approved/Update_config_process", ip, tid, "Lỗi khi cập nhật config_process", 0, "config_process");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                Log.Error(contents);
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+            catch (Exception e)
+            {
+                string contents = helper.ExceptionMessage(e);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = fdconfig_process, contents }), domainurl + "sys_process_link_approved/Update_config_process", ip, tid, "Lỗi khi cập nhật config_process", 0, "config_process");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                Log.Error(contents);
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+        }
+        [HttpDelete]
+        public async Task<HttpResponseMessage> delete_sys_process_link_approved([System.Web.Mvc.Bind(Include = "")][FromBody] List<int> id)
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+            }
+            IEnumerable<Claim> claims = identity.Claims;
+
+            try
+            {
+                string ip = getipaddress();
+                string name = claims.Where(p => p.Type == "fname").FirstOrDefault()?.Value;
+                string tid = claims.Where(p => p.Type == "tid").FirstOrDefault()?.Value;
+                string uid = claims.Where(p => p.Type == "uid").FirstOrDefault()?.Value;
+                bool ad = claims.Where(p => p.Type == "ad").FirstOrDefault()?.Value == "True";
+                string dvid = claims.Where(p => p.Type == "dvid").FirstOrDefault()?.Value;
+                string domainurl = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Host + ":" + HttpContext.Current.Request.Url.Port + "/";
+                try
+                {
+                    using (DBEntities db = new DBEntities())
+                    {
+                        var das = await db.sys_process_link_approved.Where(a => id.Contains(a.process_link_approved_id)).ToListAsync();
+                        List<string> paths = new List<string>();
+                        if (das != null)
+                        {
+                            List<sys_process_link_approved> del = new List<sys_process_link_approved>();
+                            foreach (var da in das)
+                            {
+                                del.Add(da);
+ 
+                            }
+                            if (del.Count == 0)
+                            {
+                                return Request.CreateResponse(HttpStatusCode.OK, new { err = "1", ms = "Bạn không có quyền xóa dữ liệu." });
+                            }
+                            db.sys_process_link_approved.RemoveRange(del);
+                        }
+                        await db.SaveChangesAsync();
+
+                        return Request.CreateResponse(HttpStatusCode.OK, new { err = "0" });
+                    }
+                }
+                catch (DbEntityValidationException e)
+                {
+                    string contents = helper.getCatchError(e, null);
+                    helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = id, contents }), domainurl + "sys_process_link_approved/Delete_config_process", ip, tid, "Lỗi khi xoá tem", 0, "config_process");
+                    if (!helper.debug)
+                    {
+                        contents = "";
+                    }
+                    Log.Error(contents);
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+                }
+                catch (Exception e)
+                {
+                    string contents = helper.ExceptionMessage(e);
+                    helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = id, contents }), domainurl + "sys_process_link_approved/Delete_config_process", ip, tid, "Lỗi khi xoá tem", 0, "config_process");
+                    if (!helper.debug)
+                    {
+                        contents = "";
+                    }
+                    Log.Error(contents);
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+                }
+
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
