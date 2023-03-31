b8c92d3e820a46af6e46a8b02ac206db02b74aa8
DevPhien <phienprettyboy@gmail.com>
2023-03-31 09:20:24 +0700

    commit vue

--- Vue/src/views/hrm/profile/profile.vue
+++ Vue/src/views/hrm/profile/profile.vue
@@ -354,14 +354,16 @@ const editItem = (item, str) => {
         var tbs = JSON.parse(data);
         if (tbs[0] != null && tbs[0].length > 0) {
           model.value = tbs[0][0];
-          if(model.value["birthplace_id"] == null){
+          if (model.value["birthplace_id"] == null) {
             model.value["select_birthplace"] = model.value["birthplace_name"];
           }
-          if(model.value["birthplace_origin_id"] == null){
-            model.value["select_birthplace_origin"] = model.value["birthplace_origin_name"];
+          if (model.value["birthplace_origin_id"] == null) {
+            model.value["select_birthplace_origin"] =
+              model.value["birthplace_origin_name"];
           }
-          if(model.value["place_register_permanent"] == null){
-            model.value["select_place_register_permanent"] = model.value["place_register_permanent_name"];
+          if (model.value["place_register_permanent"] == null) {
+            model.value["select_place_register_permanent"] =
+              model.value["place_register_permanent_name"];
           }
           // model.value["select_birthplace"] = {};
           // model.value["select_birthplace"][
@@ -1338,6 +1340,22 @@ onMounted(() => {
   initCount();
   initData(true);
 });
+// const test = () => {
+//   var str = encr(
+//     JSON.stringify({
+//       user: {
+//         Username: "test",
+//         Password: "123456",
+//         Email: "maiphien261299@gmail.com",
+//         FullName: "test",
+//       },
+//       isAdd: true,
+//     }),
+//     SecretKey,
+//     cryoptojs
+//   ).toString();
+//   console.log(str);
+// };
 </script>
 <template>
   <div class="surface-100 p-2">
@@ -1772,6 +1790,7 @@ onMounted(() => {
         </OverlayPanel>
       </template>
       <template #end>
+        <!-- <Button @click="test()" label="test" icon="pi pi-plus" class="mr-2" /> -->
         <Button
           @click="openAddDialog('Thêm mới hồ sơ')"
           label="Thêm mới"
