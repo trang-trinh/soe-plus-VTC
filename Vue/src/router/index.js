25b597bc306192b4e18697a965417e04efa56dcb
trangtrinhit@gmail.com <trangtrinhit@gmail.com>
2023-04-18 17:09:56 +0700

    trangtrinh

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -1983,7 +1983,7 @@ const router = createRouter({
     },
     // Request/Config
     {
-      path: "/request/config/ca_team",
+      path: "/request/config/set_auth_sign",
       name: "Request_Config_Auth_Sign",
       component: Request_Config_Auth_Sign,
     },
