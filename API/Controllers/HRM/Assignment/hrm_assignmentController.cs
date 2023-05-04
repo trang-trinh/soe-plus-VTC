1e24686e8b0d9d0a4631bdcda694375361ad5197
DevPhien <phienprettyboy@gmail.com>
2023-05-04 18:24:48 +0700

    commit api

---
+++ API/Controllers/HRM/Assignment/hrm_assignmentController.cs
@@ -0,0 +1,210 @@
+﻿using API.Models;
+using Helper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace API.Controllers.HRM.Assignment
+{
+    [Authorize(Roles = "login")]
+    public class hrm_assignmentController : ApiController
+    {
+        public string getipaddress()
+        {
+            //var host = Dns.GetHostEntry(Dns.GetHostName());
+            //foreach (var ip in host.AddressList)
+            //{
+            //    if (ip.AddressFamily == AddressFamily.InterNetwork)
+            //    {
+            //        return ip.ToString();
+            //    }
+            //}
+            return HttpContext.Current.Request.UserHostAddress;
+        }
+        [HttpPut]
+        public async Task<HttpResponseMessage> update_assignment()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            IEnumerable<Claim> claims = identity.Claims;
+            string ip = getipaddress();
+            string name = claims.Where(p => p.Type == "fname").FirstOrDefault()?.Value;
+            string tid = claims.Where(p => p.Type == "tid").FirstOrDefault()?.Value;
+            string uid = claims.Where(p => p.Type == "uid").FirstOrDefault()?.Value;
+            string domainurl = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Host + ":" + HttpContext.Current.Request.Url.Port + "/";
+            try
+            {
+                if (identity == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+            }
+            try
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    if (!Request.Content.IsMimeMultipartContent())
+                    {
+                        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                    }
+                    // Provider
+                    string rootTemp = HttpContext.Current.Server.MapPath("~/Portals");
+                    bool existsTemp = Directory.Exists(rootTemp);
+                    if (!existsTemp)
+                        Directory.CreateDirectory(rootTemp);
+                    var provider = new MultipartFormDataStreamProvider(rootTemp);
+                    var task = await Request.Content.ReadAsMultipartAsync(provider);
+
+                    // Params
+                    var user_now = await db.sys_users.AsNoTracking().FirstOrDefaultAsync(x => x.user_id == uid);
+                    bool isAdd = bool.Parse(provider.FormData.GetValues("isAdd").SingleOrDefault());
+                    var md = provider.FormData.GetValues("model").SingleOrDefault();
+                    hrm_profile_assignment model = JsonConvert.DeserializeObject<hrm_profile_assignment>(md);
+
+                    #region Model
+                    if (isAdd)
+                    {
+                        model.is_order = model.is_order ?? (db.hrm_contract.Count() + 1);
+                        model.status = 0;
+                        model.is_active = true;
+                        model.is_experiment = true;
+                        model.created_by = uid;
+                        model.created_date = DateTime.Now;
+                        model.created_ip = ip;
+                        model.created_token_id = tid;
+                        db.hrm_profile_assignment.Add(model);
+
+                        var olds = await db.hrm_profile_assignment.Where(x => x.profile_id == model.profile_id).ToListAsync();
+                        if (olds.Count > 0)
+                        {
+                            foreach(var item in olds)
+                            {
+                                item.is_active = false;
+                                item.is_experiment = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        model.modified_by = uid;
+                        model.modified_date = DateTime.Now;
+                        model.modified_ip = ip;
+                        model.modified_token_id = tid;
+                        db.Entry(model).State = EntityState.Modified;
+                    }
+                    #endregion
+                    await db.SaveChangesAsync();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { err = "0" });
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                string contents = helper.getCatchError(e, null);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = contents }), domainurl + "hrm_assignment/update_assignment", ip, tid, "Lỗi khi cập nhật", 0, "hrm_assignment");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+            catch (Exception e)
+            {
+                string contents = helper.ExceptionMessage(e);
+                helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = contents }), domainurl + "hrm_assignment/update_assignment", ip, tid, "Lỗi khi cập nhật", 0, "hrm_assignment");
+                if (!helper.debug)
+                {
+                    contents = "";
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+            }
+        }
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> delete_assignment([System.Web.Mvc.Bind(Include = "")][FromBody] List<int> ids)
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            IEnumerable<Claim> claims = identity.Claims;
+            try
+            {
+                if (identity == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { ms = "Bạn không có quyền truy cập chức năng này!", err = "1" });
+            }
+            try
+            {
+                string ip = getipaddress();
+                string name = claims.Where(p => p.Type == "fname").FirstOrDefault()?.Value;
+                string tid = claims.Where(p => p.Type == "tid").FirstOrDefault()?.Value;
+                string uid = claims.Where(p => p.Type == "uid").FirstOrDefault()?.Value;
+                bool ad = claims.Where(p => p.Type == "ad").FirstOrDefault()?.Value == "True";
+                string domainurl = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Host + ":" + HttpContext.Current.Request.Url.Port + "/";
+                try
+                {
+                    using (DBEntities db = new DBEntities())
+                    {
+                        string root = HttpContext.Current.Server.MapPath("~/Portals");
+                        var das = await db.hrm_profile_assignment.Where(a => ids.Contains(a.assignment_id)).ToListAsync();
+                        if (das != null)
+                        {
+                            List<hrm_profile_assignment> del = new List<hrm_profile_assignment>();
+                            foreach (var da in das)
+                            {
+                                del.Add(da);
+                            }
+                            if (del.Count == 0)
+                            {
+                                return Request.CreateResponse(HttpStatusCode.OK, new { err = "1", ms = "Bạn không có quyền xóa dữ liệu." });
+                            }
+                            db.hrm_profile_assignment.RemoveRange(del);
+                        }
+                        await db.SaveChangesAsync();
+                        return Request.CreateResponse(HttpStatusCode.OK, new { err = "0" });
+                    }
+                }
+                catch (DbEntityValidationException e)
+                {
+                    string contents = helper.getCatchError(e, null);
+                    helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = contents, contents }), domainurl + "hrm_assignment/delete_assignment", ip, tid, "Lỗi khi xóa", 0, "hrm_assignment");
+                    if (!helper.debug)
+                    {
+                        contents = "";
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+                }
+                catch (Exception e)
+                {
+                    string contents = helper.ExceptionMessage(e);
+                    helper.saveLog(uid, name, JsonConvert.SerializeObject(new { data = contents, contents }), domainurl + "hrm_assignment/delete_assignment", ip, tid, "Lỗi khi xoá", 0, "hrm_assignment");
+                    if (!helper.debug)
+                    {
+                        contents = "";
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, new { ms = contents, err = "1" });
+                }
+
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
