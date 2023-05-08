bea8969714fea7130e8c050ec10bf257293b81aa
Mysterious MitiS <haisesasaki1203@gmail.com>
2023-05-08 15:27:47 +0700

    commit loop detail task

---
+++ Vue/src/components/task_origin/Detail_Task/DetailLoop.vue
@@ -0,0 +1,22 @@
+<script setup>
+import DetailedChild from "../DetailedWork.vue";
+const props = defineProps({
+  keys: Intl,
+  id: String,
+  turn: Intl,
+  closeChildDetail: Function,
+});
+</script>
+<template>
+  <div>
+    <DetailedChild
+      :keys="props.keys"
+      :id="props.id"
+      :turn="props.turn"
+      :closeChildDetail="props.closeChildDetail"
+    >
+    </DetailedChild>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
