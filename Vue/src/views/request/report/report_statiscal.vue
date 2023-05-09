000262496c26ee93b86ccf4e6b479faa4f5b74f0
trangtrinhit@gmail.com <trangtrinhit@gmail.com>
2023-05-09 08:38:02 +0700

    trangtrinh

---
+++ Vue/src/views/request/report/report_statiscal.vue
@@ -0,0 +1,318 @@
+<script setup>
+import { ref, inject, onMounted, watch } from "vue";
+import { useToast } from "vue-toastification";
+import { useRoute } from "vue-router";
+import { required, maxLength } from "@vuelidate/validators";
+import { useVuelidate } from "@vuelidate/core";
+import { FilterMatchMode, FilterOperator } from "primevue/api";
+import { encr, checkURL } from "../../../util/function.js";
+
+const router = inject("router");
+const route = useRoute();
+const emitter = inject("emitter");
+const cryoptojs = inject("cryptojs");
+const axios = inject("axios");
+const store = inject("store");
+const swal = inject("$swal");
+const isDynamicSQL = ref(false);
+const toast = useToast();
+const baseUrlCheck = baseURL;
+const config = {
+    headers: { Authorization: `Bearer ${store.getters.token}` },
+};
+
+const options = ref({
+    loading: true,
+    search: '',
+    pageNo: 1,
+    pageSize: 20,
+    total: 0,
+    is_team: false,
+});
+
+const datalists = ref();
+
+const loadData = (rf) => {
+    options.value.loading = true;
+    axios
+        .post(
+            baseURL + "/api/request/getData",
+            {
+                str: encr(
+                    JSON.stringify({
+                        proc: "report_request_statistical_list",
+                        par: [
+                            { par: "user_id", va: store.getters.user.user_id },
+                            { par: "search", va: options.value.search },
+                        ],
+                    }),
+                    SecretKey,
+                    cryoptojs
+                ).toString(),
+            },
+            config
+        )
+        .then((response) => {
+            if (response != null && response.data != null) {
+                let data = JSON.parse(response.data.data)[0];
+                datalists.value = data;
+            }
+            swal.close();
+            if (options.value.loading) options.value.loading = false;
+        })
+        .catch((error) => {
+            toast.error("Tải dữ liệu không thành công!");
+            options.value.loading = false;
+            if (error && error.status === 401) {
+                swal.fire({
+                    text: "Mã token đã hết hạn hoặc không hợp lệ, vui lòng đăng nhập lại!",
+                    confirmButtonText: "OK",
+                });
+                store.commit("gologout");
+            }
+        });
+};
+
+onMounted(() => {
+    loadData(true);
+    return {
+        datalists,
+        loadData,
+    };
+});
+</script>
+<template>
+    <div class="surface-100 p-2">
+        <Toolbar class="outline-none surface-0 border-none">
+            <template #start>
+                <div class="flex" style="width: 100%;">
+                    <span class="p-input-icon-left"
+                        style="position: relative;width: 50%;display: flex;align-items: center;">
+                        <InputSwitch @change="ChangeIsDepartment()" class="col-6" style="position: absolute;"
+                            v-model="options.is_team" />
+                        <label class="col-3 text-left p-0" style="position: absolute; left: 50px">Hiện team</label>
+                    </span>
+                    <span class="p-input-icon-left" style="width: 50%;">
+                        <i class="pi pi-search" />
+                        <InputText type="text" spellcheck="false" v-model="options.search" style="width: 75%;"
+                            placeholder="Tìm kiếm" v-on:keyup.enter="loadData(true)" />
+                    </span>
+                </div>
+            </template>
+            <template #end>
+                <Button @click="refresh()" class="p-button-outlined p-button-secondary mr-2" icon="pi pi-file"
+                    label="Export" />
+                <Button @click="refresh()" class="p-button-outlined p-button-secondary mr-2" icon="pi pi-refresh"
+                    label="Tải lại" />
+            </template>
+        </Toolbar>
+        <div class="flex"
+            style="width: 100%;height: 50px;justify-content: center;align-items: center;font-weight: bold;font-size: 14px;">
+            BÁO CÁO THỐNG KÊ TỔNG HỢP ĐỀ XUẤT
+        </div>
+        <!-- <div class="d-lang-table">
+            <DataTable class="table-request-data" :value="datalists" :scrollable="true" selectionMode="single"
+                dataKey="request_id" scrollHeight="calc(100vh - 170px)" :rowHover="true" v-model:selection="selectedNodes">
+                <Column field="request_form_name" header="Form" headerStyle="text-align:left;height:45px"
+                    bodyStyle="text-align:left;" class="align-items-center">
+                </Column>
+                <Column field="count_all_request" header="Tổng số yêu cầu" headerStyle="text-align:center;max-width:140px;height:45px"
+                    bodyStyle="text-align:center;max-width:140px;"
+                    class="align-items-center justify-content-center text-center">
+                </Column>
+                <Column field="created_by" header="Người tạo" headerStyle="text-align:center;max-width:120px;height:45px"
+                    bodyStyle="text-align:center;max-width:120px;"
+                    class="align-items-center justify-content-center text-center">
+                </Column>
+                <Column field="listSignUser" header="Người duyệt"
+                    headerStyle="text-align:center;max-width:250px;height:45px"
+                    bodyStyle="text-align:center;max-width:250px;"
+                    class="align-items-center justify-content-center text-center">
+                </Column>
+                <Column field="status" header="Trạng thái" headerStyle="text-align:center;max-width:180px;height:45px"
+                    bodyStyle="text-align:center;max-width:180px;"
+                    class="align-items-center justify-content-center text-center">
+                </Column>
+                <Column header="" headerStyle="text-align:center;max-width:50px"
+                    bodyStyle="text-align:center;max-width:50px"
+                    class="align-items-center justify-content-center text-center" v-if="options.is_func">
+                </Column>
+                <template #empty>
+                    <div class="align-items-center justify-content-center p-4 text-center m-auto" :style="{
+                        display: 'flex',
+                        width: '100%',
+                        height: 'calc(100vh - 230px)',
+                        backgroundColor: '#fff',
+                    }">
+                        <div v-if="options.total == 0">
+                            <img src="../../../assets/background/nodata.png" height="144" />
+                            <h3 class="m-1">Không có dữ liệu</h3>
+                        </div>
+                    </div>
+                </template>
+            </DataTable>
+        </div> -->
+        <div
+            style="overflow: scroll;max-height: calc(100vh - 124px);min-height: calc(100vh - 124px);background-color: #fff;">
+            <table cellspacing=0 id="table-bc" class="table table-condensed table-hover tbpad" style="width: 100%;">
+                <thead style="position: sticky; z-index: 6; top:0">
+                    <tr>
+                        <th class="text-center" rowspan="3" width="200">Form</th>
+                        <th class="text-center" rowspan="2" width="100">Tổng số yêu cầu</th>
+                        <th class="text-center" rowspan="2" width="100">Mới lập</th>
+                        <th class="text-center" rowspan="2" width="100">Chờ duyệt</th>
+                        <th class="text-center" rowspan="2" width="100">Hoàn thành</th>
+                        <th class="text-center" colspan="3">Quá hạn</th> 
+                    </tr>
+                    <tr>
+                        <th class="text-center" width="100">Quá hạn chờ duyệt</th>
+                        <th class="text-center" width="100">Quá hạn đã duyệt</th>
+                        <th class="text-center" width="100">Tổng cộng</th>
+                    </tr>
+                    <tr>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                        <th class="text-center" width="100"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(d, index) in datalists" :key="index" class="item-hover">
+                        <td align="left" class="td-form">{{ d.request_form_name }}</td>
+                        <td align="center">{{ d.count_all_request }}</td>
+                        <td align="center">{{ d.count_all_request_moitao }}</td>
+                        <td align="center">{{ d.count_all_request_choduyet }}</td>
+                        <td align="center">{{ d.count_all_request_hoanthanh }}</td>
+                        <td align="center"></td>
+                        <td align="center"></td>
+                        <td align="center"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+::v-deep(.p-toolbar) {
+    .p-toolbar-group-left {
+        width: 70%;
+    }
+}</style>
+<style scoped>
+.item-hover:hover{
+  background-color: #f0f8ff!important;
+}
+.bg-group{
+  background-color: rgb(222, 230, 240) !important;
+}
+.bg-stt{
+  background-color: #e6e6e6;
+}
+.active-item{
+  background-color: #ffd95f;
+}
+.active-border{
+  border:2px solid #008eff !important
+}
+  .table {
+      margin-bottom: 0px !important;
+  }
+
+
+  .left-sticky1 {
+      position: sticky;
+      z-index: 5;
+      vertical-align: middle !important;
+  }
+
+  .left-1 {
+      left:-1px;
+  }
+  .left-2 {
+      left: 49px;
+  }
+  .left-3 {
+      left: 149px;
+  }
+  .left-4 {
+      left: 299px;
+  }
+  .btn.btn-secondary:hover {
+      background-color: #e6f0f8 !important;
+      color: #2f90d1 !important;
+  }
+
+  table{
+    border: 0.3px solid rgba(0,0,0,.3) !important;
+  }
+
+  tr td {
+      word-break: break-word;
+      vertical-align: middle !important;
+      cursor: pointer;
+  }
+
+  table th {
+      background-color: #8BCFFB !important;
+  }
+  table .td-form {
+      background-color: #f6ddcc !important;
+  }
+
+  table th, table td {
+      border: 0.3px solid rgba(0,0,0,.3) !important;
+  }
+
+</style>
+<style scoped>
+/* #table-bc{
+max-height: calc(100vh - 110px);
+overflow-y: auto;
+overflow-x: scroll;
+
+} */
+th,
+td {
+  background: #fff;
+  padding: 0.6rem;
+}
+
+.row-parent {
+  font-weight: bold;
+  margin-left: 0.5em;
+}
+
+.row-child {
+  cursor: pointer;
+  margin-left: 1.5em;
+}
+
+.row-child:hover {
+  color: #0078d4;
+}
+.toolbar-filter {
+border: unset;
+outline: unset;
+background-color: #fff;
+padding-bottom: 0px;
+}
+td.bg-group>b {
+  position: sticky;
+  left: 10px;
+}
+</style>
+<style lang="scss" scoped>
+::v-deep(.p-rowgroup-header) {
+td {
+  flex:1 1 0 !important;
+}
+}
+::v-deep(.p-datatable-emptymessage) {
+td {
+  flex:1 1 0 !important;
+}
+}
+</style>
