2e9ac0dae715b7644b3344d4d6d9369a699860c3
duytrangcac <it.duyls@gmail.com>
2023-05-10 12:01:21 +0700

    duy add

---
+++ Vue/src/views/hrm/template/components/EmptyComponent.vue
@@ -0,0 +1,21 @@
+<script>
+import { onMounted } from 'vue';
+export default ({
+    props: {
+        message: String
+    },
+    setup(props) {
+        onMounted(() => {
+        });
+        return {
+            props,
+        }
+    },
+})
+</script>
+<template>
+    <div class="text-center p-4">
+        {{ props.message || "Chưa có bản ghi nào" }}
+    </div>
+</template>
+<!-- <style lang="scss" scoped></style> -->
